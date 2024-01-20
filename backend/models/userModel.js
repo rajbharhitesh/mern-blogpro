43abf6a74f1bb27db76593c0512c2cbd0631e713
Hitesh <h08rajbhar@gmail.com>
2024-01-20 13:43:32 +0530

    user schema

---
+++ backend/models/userModel.js
@@ -0,0 +1,48 @@
+import mongoose from 'mongoose';
+
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      trim: true,
+      minlength: 2,
+      maxlength: 100,
+    },
+    email: {
+      type: String,
+      required: true,
+      trim: true,
+      minlength: 5,
+      maxlength: 100,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      trim: true,
+      minlength: 8,
+    },
+    profilePhoto: {
+      type: Object,
+      default: {
+        url: 'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460__480.png',
+        publicId: null,
+      },
+    },
+    bio: {
+      type: String,
+    },
+    isAdmin: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const User = mongoose.model('User', userSchema);
+
+export default User;

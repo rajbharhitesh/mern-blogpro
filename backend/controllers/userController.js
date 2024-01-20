81f4a1a1f3d01c985434e19488e851474f9f0112
Hitesh <h08rajbhar@gmail.com>
2024-01-20 20:08:08 +0530

    upload photo to cloudinary and setup

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,5 +1,11 @@
 import asyncHandler from '../middlewares/asyncHandler.js';
+import path from 'path';
+import fs from 'fs';
 import { User, validateUpdateUser } from '../models/userModel.js';
+import {
+  cloudinaryRemoveImage,
+  cloudinaryUploadImage,
+} from '../utils/cloudinary.js';
 
 /**-----------------------------------------------
  * @desc     Get All Users Profile
@@ -88,6 +94,38 @@ const profilePhotoUploader = asyncHandler(async (req, res) => {
   if (!req.file) {
     return res.status(400).json({ message: 'no file provided' });
   }
+
+  // 2. Get the path to the image
+  const __dirname = path.resolve();
+  const imagePath = path.join(__dirname, `/images/${req.file.filename}`);
+
+  // 3. Upload to cloudinary
+  const result = await cloudinaryUploadImage(imagePath);
+
+  // 4. Get the user from DB
+  const user = await User.findById(req.user.id);
+
+  // 5. Delete the old profile photo if exist
+  if (user.profilePhoto.publicId !== null) {
+    await cloudinaryRemoveImage(user.profilePhoto.publicId);
+  }
+
+  // 6. Change the profilePhoto field in the DB
+  user.profilePhoto = {
+    url: result.secure_url,
+    publicId: result.public_id,
+  };
+
+  await user.save();
+
+  // 7. Send response to client
+  res.status(200).json({
+    message: 'your profile photo uploaded successfully',
+    profilePhoto: { url: result.secure_url, publicId: result.public_id },
+  });
+
+  // 8. Remove image from the server
+  fs.unlinkSync(imagePath);
 });
 
 export {

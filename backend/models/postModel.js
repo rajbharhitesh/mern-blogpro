52fa839d439fa2d44ac9e645a2813497a0b0609d
Hitesh <h08rajbhar@gmail.com>
2024-01-21 11:07:51 +0530

    create post model

---
+++ backend/models/postModel.js
@@ -0,0 +1,68 @@
+import mongoose from 'mongoose';
+import Joi from 'joi';
+
+const postSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      trim: true,
+      minlength: 2,
+      maxlength: 100,
+    },
+    description: {
+      type: String,
+      required: true,
+      trim: true,
+      minlength: 2,
+      maxlength: 100,
+    },
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: 'User',
+    },
+    category: {
+      type: String,
+      required: true,
+    },
+    image: {
+      type: Object,
+      default: {
+        url: '',
+        publicId: null,
+      },
+    },
+    likes: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+      },
+    ],
+  },
+  { timestamps: true }
+);
+
+// Validate Create Post
+function validateCreatePost(obj) {
+  const schema = Joi.object({
+    title: Joi.string().trim().min(2).max(200).required(),
+    description: Joi.string().trim().min(10).required(),
+    category: Joi.string().trim().required(),
+  });
+  return schema.validate(obj);
+}
+
+// Validate Update Post
+function validateUpdatePost(obj) {
+  const schema = Joi.object({
+    title: Joi.string().trim().min(2).max(200),
+    description: Joi.string().trim().min(10),
+    category: Joi.string().trim(),
+  });
+  return schema.validate(obj);
+}
+
+const Post = mongoose.model('Post', userSchema);
+
+export { Post, validateCreatePost, validateUpdatePost };

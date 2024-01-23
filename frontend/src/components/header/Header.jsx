981bd66eec3dd95a0218a1d849ae052be229d03a
Hitesh <h08rajbhar@gmail.com>
2024-01-23 21:03:41 +0530

    frontend setup

---
+++ frontend/src/components/header/Header.jsx
@@ -0,0 +1,18 @@
+const Header = () => {
+  return (
+    <header className="header">
+      <div className="header-left">
+        <div className="header-logo">
+          <strong>BLOG</strong>
+          <i className="bi bi-pencil"></i>
+        </div>
+        <div className="header-menu">
+          <i className="bi bi-list"></i>
+        </div>
+      </div>
+      <div className="header-right"></div>
+    </header>
+  );
+};
+
+export default Header;

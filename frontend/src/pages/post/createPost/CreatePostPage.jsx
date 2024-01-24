aa7edc1b52003941a651b4ea070d56f6e1d914c0
Hitesh <h08rajbhar@gmail.com>
2024-01-24 20:32:24 +0530

    create post form

--- frontend/src/pages/post/createPost/CreatePostPage.jsx
+++ frontend/src/pages/post/createPost/CreatePostPage.jsx
@@ -1,5 +1,45 @@
+import './CreatePost.css';
+
 const CreatePostPage = () => {
-  return <div></div>;
+  return (
+    <div className="section create-post">
+      <h1 className="create-post-title">Create New Post</h1>
+
+      <form className="create-post-form">
+        <input
+          type="text"
+          name="title"
+          placeholder="post title"
+          className="create-post-input"
+        />
+
+        <select className="create-post-input">
+          <option disabled value="">
+            Select A Category
+          </option>
+          <option value="music">music</option>
+          <option value="coffee">coffee</option>
+        </select>
+
+        <textarea
+          className="create-post-textarea"
+          rows="5"
+          placeholder="Post Description"
+          name="description"
+        ></textarea>
+
+        <input
+          type="file"
+          name="file"
+          id="file"
+          className="create-post-upload"
+        />
+        <button type="submit" className="create-post-btn">
+          Create
+        </button>
+      </form>
+    </div>
+  );
 };
 
 export default CreatePostPage;
